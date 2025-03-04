dd85312948c77af6c6cd5991bd05fce6b56f9b44
Matías Iglesias <miglesias@campus.ungs.edu.ar>
2025-03-04 03:28:16 -0300

    Se avanza con formulario de configuración del sistema

--- frontend/src/app/presentation/pages/system/system.component.ts
+++ frontend/src/app/presentation/pages/system/system.component.ts
@@ -3,13 +3,37 @@ import { CommonModule } from "@angular/common";
 import { IconsModule } from "../../../utils/tabler-icons.module";
 import { MatToolbarModule } from "@angular/material/toolbar";
 import { PageToolbarComponent } from "../../components/page-toolbar/page-toolbar.component";
+import { MatFormFieldModule } from "@angular/material/form-field";
+import {
+  FormControl,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from "@angular/forms";
+import { MatInputModule } from "@angular/material/input";
+import { MatCardModule } from "@angular/material/card";
 
 @Component({
   selector: "app-system",
   templateUrl: "./system.component.html",
   standalone: true,
-  imports: [CommonModule, IconsModule, MatToolbarModule, PageToolbarComponent],
+  imports: [
+    CommonModule,
+    IconsModule,
+    MatToolbarModule,
+    PageToolbarComponent,
+    MatFormFieldModule,
+    ReactiveFormsModule,
+    MatInputModule,
+    MatCardModule,
+  ],
 })
 export class SystemComponent {
-  constructor() {}
+  tokenForm: FormGroup;
+  constructor() {
+    this.tokenForm = new FormGroup({
+      expiration: new FormControl<number>(0, [Validators.required]),
+      failedAttempts: new FormControl<number>(0, [Validators.required]),
+    });
+  }
 }
